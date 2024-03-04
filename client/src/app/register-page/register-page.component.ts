2f748283c946748bace6ca449f1cd659af4f8566
Jomin Rajesh K <Jomin.Rajesh.K1@ibm.com>
2024-03-04 16:05:31 +0530

    tsconfig updated

--- client/src/app/register-page/register-page.component.ts
+++ client/src/app/register-page/register-page.component.ts
@@ -17,7 +17,6 @@ constructor(public http: HttpClient)
 {
 
 }
-public showEmailError = false;
 public showRequired = false;
 signUpForm = new FormGroup({
   email: new FormControl('',[Validators.email,Validators.required]),
@@ -34,8 +33,7 @@ onSubmit()
   console.log(this.signUpForm.value)
   console.log(this.signUpForm.valid)
   console.log(this.signUpForm.controls.email.errors)
-  this.showEmailError = false;
-  this.showRequired = false
+  this.showRequired = true
   if(!this.signUpForm.valid)
   {
     return;
