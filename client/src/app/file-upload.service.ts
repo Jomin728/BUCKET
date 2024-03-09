bd044e43f919c020beceb4b181ca22c1c974a4de
Jomin Rajesh K <Jomin.Rajesh.K1@ibm.com>
2024-03-09 09:36:23 +0530

    08/04/24

--- client/src/app/file-upload.service.ts
+++ client/src/app/file-upload.service.ts
@@ -10,7 +10,7 @@ export class FileUploadService {
   // public httpHeader = new HttpHeaders().set("Content-Type","multipart/form-data")
   uploadFile(fileData)
   {
-    return this.httpClient.post('http://localhost:8000/fileUpload',fileData)
+    return this.httpClient.post('http://localhost:8000/file-upload',fileData)
   }
   // multipart/form-data; boundary=----WebKitFormBoundaryiPoPqJOjhHyzFhk3
   // {headers:this.httpHeader}
