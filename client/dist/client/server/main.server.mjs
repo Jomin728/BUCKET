e0f45dfa3ddea13cb68bf389594e9871dd05ca3e
Jomin Rajesh K <Jomin.Rajesh.K1@ibm.com>
2024-04-07 22:52:22 +0530

    build updated

--- client/dist/client/server/main.server.mjs
+++ client/dist/client/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-D74YBCG3.mjs";import"./chunk-JNSAMXJP.mjs";import"./chunk-RTFX5YZN.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-G3DIFAHJ.mjs";import"./chunk-JNSAMXJP.mjs";import"./chunk-RTFX5YZN.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
